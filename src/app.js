21114e5d3c42a5bb5cbc6a3eafb717cdd8d9b539
Edier2 <ediercuenca2712@gmail.com>
2026-07-05 17:18:21 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,4 +18,11 @@ app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
+const connection = mysql.createConnection({
+    host:     process.env.DB_HOST,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
 export default app;
