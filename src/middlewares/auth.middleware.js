0c48abf29e204e0c5fa76b4d724cb2520126d877
Edier2 <ediercuenca2712@gmail.com>
2026-07-05 23:48:06 -0500

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,14 +1,11 @@
 import jwt from 'jsonwebtoken';
-
 export const verificarToken = (req, res, next) => {
     const token = req.headers.authorization;
-
     if (!token) {
         return res.status(401).json({ error: 'Token requerido' });
     }
-
     try {
-        const decoded = jwt.verify(token.split(' ')[1], 'secreto_sena');
+        const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
